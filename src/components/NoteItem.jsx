97229aa76174808128b4cd663aada4db598c9282
unknown <alwyyourbae@gmail.com>
2023-12-04 23:58:20 -0800

    update NoteItem.jsx

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ArchiveButton from "./ArchiveButton";
 import DeleteButton from "./DeleteButton";
 
-function NoteItem({ id, onDelete, onArchive, title, date, body }) {
+function NoteItem({ id, onDelete, onArchive, title, createdAt, body }) {
   return (
     <div className="note-item">
       <div className="note-item__content">
         <h1 className="note-item__title">{title}</h1>
-        <p className=".note-item__date">{date}</p>
+        <p className=".note-item__date">{createdAt}</p>
         <p className="note-item__body">{body}</p>
         <div className="note-item__action">
           <DeleteButton id={id} onDelete={onDelete} />
