441b4cd495eff702f3f2c52b298a8e189c4eddfc
unknown <alwyyourbae@gmail.com>
2023-12-05 22:26:57 -0800

    fix jsx

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import ArchiveButton from "./ArchiveButton";
 import DeleteButton from "./DeleteButton";
-import { showFormattedDate } from "../utils"
+import { showFormattedDate } from "../utils";
 
-function NoteItem({
-  note,
-  onDelete,
-  onArchive,
-}) {
+function NoteItem({ note, onDelete, onArchive }) {
   return (
     <div className="note-item">
       <div className="note-item__content">
-        <h1 className="note-item__title">{note.title}</h1>
-        <p className=".note-item__date">{showFormattedDate(note.createdAt)}</p>
+        <h3 className="note-item__title">{note.title}</h3>
+        <p className="note-item__date">{showFormattedDate(note.createdAt)}</p>
         <p className="note-item__body">{note.body}</p>
-        <div className="note-item__action">
-          <DeleteButton id={note.id} onDelete={onDelete} />
-          <ArchiveButton id={note.id} archived={note.archived} onArchive={onArchive} />
-        </div>
+      </div>
+      <div className="note-item__action">
+        <DeleteButton id={note.id} onDelete={onDelete} />
+        <ArchiveButton
+          id={note.id}
+          archived={note.archived}
+          onArchive={onArchive}
+        />
       </div>
     </div>
   );
