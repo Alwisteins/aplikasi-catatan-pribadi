ea88c1d68d71e353680159124b44be042c892154
unknown <alwyyourbae@gmail.com>
2023-12-05 21:46:34 -0800

    rename isArchived props to archived

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ArchiveButton from "./ArchiveButton";
 import DeleteButton from "./DeleteButton";
+import { showFormatedDate } from "../utils"
 
 function NoteItem({
   note,
   onDelete,
   onArchive,
-  isArchived,
 }) {
   return (
     <div className="note-item">
       <div className="note-item__content">
         <h1 className="note-item__title">{note.title}</h1>
-        <p className=".note-item__date">{note.createdAt}</p>
+        <p className=".note-item__date">{showFormatedDate(note.createdAt)}</p>
         <p className="note-item__body">{note.body}</p>
         <div className="note-item__action">
           <DeleteButton id={note.id} onDelete={onDelete} />
