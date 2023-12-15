f42d180a9441ed0172e6a43e82a09da4ba329c21
unknown <alwyyourbae@gmail.com>
2023-12-15 14:45:10 -0800

    change width

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import Collections from "./collections";
 
 export default function Sidebar() {
   return (
-    <div className="box-border w-80 h-screen p-6 border-r-2 border-[#D9D9D9]">
+    <div className="box-border w-1/4 h-screen p-6 border-r-2 border-[#D9D9D9]">
       <div className="flex items-center">
         <img src="/notes.svg" alt="logo" className="w-12" />
         <h1 className="font-bold text-3xl">ZETTELKASTEN</h1>
