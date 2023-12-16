7b196259451b17fbe3ee722687de7dd771d01e5a
unknown <alwyyourbae@gmail.com>
2023-12-16 05:02:04 -0800

    change font weight button

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar({ onSearch }) {
   return (
     <div className="h-16 w-5/6 px-6 border-b-2 flex items-center">
       <div className="flex space-x-4">
-        <Button variant='contained' size="large" sx={{ backgroundColor: '#A855F7', fontWeight: '800', borderRadius: '10px', fontSize: '14px' }}>+ Add Note</Button>
+        <Button variant='contained' size="large" sx={{ backgroundColor: '#A855F7', fontWeight: '700', borderRadius: '10px', fontSize: '14px' }}>+ Add Note</Button>
         <input
           type="text"
           placeholder="Search note here..."
