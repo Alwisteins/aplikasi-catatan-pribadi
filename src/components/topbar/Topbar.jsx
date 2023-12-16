d34ef1c4133ca599b80b3da546022ca91a8e0077
unknown <alwyyourbae@gmail.com>
2023-12-16 05:01:05 -0800

    replace botton style using material ui

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import { Button } from "@mui/material";
 import ToggleButton from "./ToggleButton";
 
 export default function Topbar({ onSearch }) {
   return (
     <div className="h-16 w-5/6 px-6 border-b-2 flex items-center">
       <div className="flex space-x-4">
-        <button className="w-[133px] h-[40px] shadow-[1px_2px_5px_2px_rgba(0,0,0,0.25)] rounded-lg font-medium">
-          + Add Note
-        </button>
+        <Button variant='contained' size="large" sx={{ backgroundColor: '#A855F7', fontWeight: '800', borderRadius: '10px', fontSize: '14px' }}>+ Add Note</Button>
         <input
           type="text"
           placeholder="Search note here..."
