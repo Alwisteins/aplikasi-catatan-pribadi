23309c66ab84d22a0334862389e26e7ee7c8caf3
unknown <alwyyourbae@gmail.com>
2023-12-16 17:56:24 -0800

    add property to Search component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,10 +5,10 @@ import ToggleButton from "./ToggleButton";
 
 export default function Topbar({ onSearch }) {
   return (
-    <div className="h-16 w-5/6 px-6 border-b-2 flex items-center">
+    <div className="h-16 w-5/6 px-6 border-b-2 flex items-center justify-between">
       <div className="flex space-x-4">
         <AddButton />
-        <Search />
+        <Search onSearch={onSearch} />
       </div>
       <div>
         <ToggleButton />
