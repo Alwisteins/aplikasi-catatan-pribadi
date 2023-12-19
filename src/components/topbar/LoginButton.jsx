dd39e26814f808dbb841cbc964225988814c5db9
unknown <alwyyourbae@gmail.com>
2023-12-18 18:56:43 -0800

    update login button

--- src/components/topbar/LoginButton.jsx
+++ src/components/topbar/LoginButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import {
   GoogleLoginButton,
   GithubLoginButton,
@@ -7,23 +8,34 @@ import { Button } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 
 export default function LoginButton() {
+  const [isHidden, setIsHidden] = useState(true);
+  const handleOnClick = () => {
+    setIsHidden(!isHidden);
+  };
   return (
-    <Button
-      variant="contained"
-      size="medium"
-      sx={{
-        backgroundColor: "#A855F7",
-        fontWeight: "600",
-        borderRadius: "10px",
-        fontSize: "14px",
-        boxShadow: "none",
-        "&:hover": {
-          backgroundColor: "#9333ea",
+    <div>
+      <Button
+        variant="contained"
+        size="medium"
+        sx={{
+          backgroundColor: "#A855F7",
+          fontWeight: "600",
+          borderRadius: "10px",
+          fontSize: "14px",
           boxShadow: "none",
-        },
-      }}
-    >
-      <LoginIcon style={{ paddingRight: "6px" }} /> Sign in
-    </Button>
+          "&:hover": {
+            backgroundColor: "#9333ea",
+            boxShadow: "none",
+          },
+        }}
+        onClick={handleOnClick}
+      >
+        <LoginIcon style={{ paddingRight: "6px" }} /> Sign in
+      </Button>
+      <div className={`absolute top-64 right-96 ${isHidden ? "hidden" : ""}`}>
+        <GoogleLoginButton />
+        <GithubLoginButton  />
+      </div>
+    </div>
   );
 }
