7f049809044bc8f4755e3e38e09daccf8219533b
unknown <alwyyourbae@gmail.com>
2024-02-01 08:52:43 -0800

    refactor(sidebar.jsx colelctions.jsx): change properties name

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Collections from "./collections";
 
-export default function Sidebar() {
+export default function Sidebar({ collectionList, handleCollectionModalOpen }) {
   return (
     <div className="box-border w-1/4 h-screen p-6 border-r-2 border-[#D9D9D9]">
       <div className="flex items-center">
         <img src="/notes.svg" alt="logo" className="w-12" />
         <h1 className="font-bold text-3xl">ZETTELKASTEN</h1>
       </div>
-      <Collections />
+      <Collections collectionList={collectionList} handleCollectionModalOpen={handleCollectionModalOpen}  />
     </div>
   );
 }
